0d148d9a1505d666ef86cc4bfae0c4387c0ffb0d
Miah <sowhoyou@gmail.com>
2022-07-03 03:31:24 -0400

    Update confetti.js

--- assets/confetti.js
+++ assets/confetti.js
@@ -45,7 +45,7 @@ var removeConfetti; //call to stop the confetti animation and remove all confett
 			canvas = document.createElement("canvas");
 			canvas.setAttribute("id", "confetti-canvas");
 			canvas.setAttribute("style", "display:block;z-index:999999;pointer-events:none");
-			document.body.appendChild(canvas);
+			document.body.prepend(canvas);
 			canvas.width = width;
 			canvas.height = height;
 			window.addEventListener("resize", function() {

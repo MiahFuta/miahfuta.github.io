2b890b84a73961f1f7b96967f374b1686d088038
SoWhoYou <sowhoyou@gmail.com>
2022-07-13 08:15:59 -0400

    Update menu.js

--- assets/menu.js
+++ assets/menu.js
@@ -1,13 +1,17 @@
-$('input[type=checkbox]').change( function(){
-    if (this.checked) {
-        $("#menu").css("width", "50px");
-    } else {
-        $("#menu").css("width", "200px");
-    }
-});
-
 $(window).on('load', function () {
+
     setTimeout(function() {
+
         $("#menu").css("width", "50px");
+
+        $('input[type=checkbox]').change( function(){
+            if (this.checked) {
+                $("#menu").css("width", "50px");
+            } else {
+                $("#menu").css("width", "200px");
+            }
+        });
+
     }, 2000);
+    
 });

513403915f59cff2e524ef4d8de0c9297b3ea609
SoWhoYou <sowhoyou@gmail.com>
2022-07-13 07:47:11 -0400

    updated

--- assets/menu.js
+++ assets/menu.js
@@ -1,15 +1,26 @@
 $(document).ready(function () {
 
-    document.getElementById("menu").style.width = "50px";
+    // document.getElementById("menu").style.width = "50px";
 
-    var checkbox = document.querySelector("input[name=checkbox]");
+    // var checkbox = document.querySelector("input[name=checkbox]");
 
-    checkbox.addEventListener('change', function() {
-    if (this.checked) {
-        document.getElementById("menu").style.width = "50px";
-    } else {
-        document.getElementById("menu").style.width = "200px";
-    }
-    });
+    // checkbox.addEventListener('change', function() {
+    // if (this.checked) {
+    //     document.getElementById("menu").style.width = "50px";
+    // } else {
+    //     document.getElementById("menu").style.width = "200px";
+    // }
+    // });
+
+    $("#menu").css("width", "50px");
+
+    $('input[name=checkbox]').change(
+        function(){
+            if (this.checked) {
+                $("#menu").css("width", "50px");
+            } else {
+                $("#menu").css("width", "200px");
+            }
+        });
 
 });

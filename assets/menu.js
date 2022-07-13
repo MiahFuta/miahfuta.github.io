8e6e1b12e8642ec4227cc375bc0b6f253f378663
SoWhoYou <sowhoyou@gmail.com>
2022-07-13 06:30:38 -0400

    updates

---
+++ assets/menu.js
@@ -0,0 +1,12 @@
+includeHTML();
+
+document.getElementById("menu").style.width = "50px";
+var checkbox = document.querySelector("input[name=checkbox]");
+checkbox.addEventListener('change', function() {
+  if (this.checked) {
+    document.getElementById("menu").style.width = "50px";
+  } else {
+    document.getElementById("menu").style.width = "200px";
+  }
+});
+

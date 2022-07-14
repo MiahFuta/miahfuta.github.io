11c06aba57121322363aaa71c2073addb4fcb700
SoWhoYou <sowhoyou@gmail.com>
2022-07-13 22:47:09 -0400

    updates

--- assets/js/ebike.js
+++
@@ -1,9 +0,0 @@
-$(window).on('load', function () {
-    $(function() {
-        $('.carousel.lazy-load').bind('slide.bs.carousel', function (e) {
-            var image = $(e.relatedTarget).find('img[data-src]');
-            image.attr('src', image.data('src'));
-            image.removeAttr('data-src');
-        });
-    });
-});

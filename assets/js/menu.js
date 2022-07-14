4d2cf2a8f1629230705e26effca68dc6a9997c52
SoWhoYou <sowhoyou@gmail.com>
2022-07-14 18:58:06 -0400

    updates

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,15 +1,12 @@
 $(window).on('load', function () {
     setTimeout(function() {
 
-        // $("#menu").css("width", "50px");
         $("#menu").css("opacity", "0.5");
 
         $('#menu :checkbox').change( function(){
             if (this.checked) {
-                // $("#menu").css("width", "50px");
                 $("#menu").css("opacity", "0.5");
             } else {
-                // $("#menu").css("width", "100px");
                 $("#menu").css("opacity", "1");
             }
         });

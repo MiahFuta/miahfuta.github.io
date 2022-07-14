b254ef7337d69706e2f2e3c9415ee044faf81b4c
SoWhoYou <sowhoyou@gmail.com>
2022-07-14 05:43:55 -0400

    Update header.js

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,5 +1,4 @@
 $(function(){
-    // $("#includeMenu").load("menu.html");
     $("div[data-includeHTML]").each(function () {                
         $(this).load("imports/" + $(this).attr("data-includeHTML"));
     });

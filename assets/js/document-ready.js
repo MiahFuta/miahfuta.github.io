98f4832af709f7086e41cf7eb8e229d0e67dcc9d
SoWhoYou <sowhoyou@gmail.com>
2022-07-14 18:54:22 -0400

    updates

--- assets/js/document-ready.js
+++ assets/js/document-ready.js
@@ -1,20 +1,28 @@
 $(window).on('load', function () {
     setTimeout(function() {
 
-        if(jQuery.browser.mobile) {
-            $("#menu").css("width", "50px");
-            $("#menu").css("opacity", "0.5");
+        // $("#menu").css("width", "50px");
+        $("#menu").css("opacity", "0.5");
 
-            $('#menu :checkbox').change( function(){
-                if (this.checked) {
-                    $("#menu").css("width", "50px");
-                    $("#menu").css("opacity", "0.5");
-                } else {
-                    $("#menu").css("width", "100px");
-                    $("#menu").css("opacity", "1");
-                }
-            });
-        }
+        $('#menu :checkbox').change( function(){
+            if (this.checked) {
+                // $("#menu").css("width", "50px");
+                $("#menu").css("opacity", "0.5");
+            } else {
+                // $("#menu").css("width", "100px");
+                $("#menu").css("opacity", "1");
+            }
+        });
+
+        $("#menu").on("mouseover", function () {
+            $("#menu").css("opacity", "1");
+        });
+        
+        $("#menu").on("mouseleave", function () {
+            if ( $('input#collapse').is(':checked') ) {
+                $("#menu").css("opacity", "0.5");
+            }
+        });
 
     }, 100);
 });
--- assets/js/menu.js
+++
@@ -1,9 +0,0 @@
-$("#menu").on("mouseover", function () {
-    $("#menu").css("opacity", "1");
-});
-
-$("#menu").on("mouseleave", function () {
-    if ( $('input#collapse').is(':checked') ) {
-        $("#menu").css("opacity", "0.5");
-    }
-});

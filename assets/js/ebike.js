b50c031f946e9a5a38c6933a649c6d5313aec15a
SoWhoYou <sowhoyou@gmail.com>
2022-07-14 00:57:44 -0400

    updates

---
+++ assets/js/ebike.js
@@ -0,0 +1,16 @@
+$('.card-body').mouseenter(function () { 
+    $('.carousel-control-next-icon').css('display','');
+    $('.carousel-control-prev-icon').css('display','');
+});
+
+$('.card-body').mouseleave(function () { 
+    $('.carousel-control-next-icon').css('display','none');
+    $('.carousel-control-prev-icon').css('display','none');
+});
+
+$(document).on('click', '[data-toggle="lightbox"]', function(event) {
+    event.preventDefault();
+    $(this).ekkoLightbox({
+        alwaysShowClose: true
+    });
+});

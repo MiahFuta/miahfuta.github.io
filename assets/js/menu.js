db98c8a36c365d251c1b1baff80574f86da6a0d2
SoWhoYou <sowhoyou@gmail.com>
2022-07-14 18:55:25 -0400

    updates

---
+++ assets/js/menu.js
@@ -0,0 +1,28 @@
+$(window).on('load', function () {
+    setTimeout(function() {
+
+        // $("#menu").css("width", "50px");
+        $("#menu").css("opacity", "0.5");
+
+        $('#menu :checkbox').change( function(){
+            if (this.checked) {
+                // $("#menu").css("width", "50px");
+                $("#menu").css("opacity", "0.5");
+            } else {
+                // $("#menu").css("width", "100px");
+                $("#menu").css("opacity", "1");
+            }
+        });
+
+        $("#menu").on("mouseover", function () {
+            $("#menu").css("opacity", "1");
+        });
+        
+        $("#menu").on("mouseleave", function () {
+            if ( $('input#collapse').is(':checked') ) {
+                $("#menu").css("opacity", "0.5");
+            }
+        });
+
+    }, 100);
+});

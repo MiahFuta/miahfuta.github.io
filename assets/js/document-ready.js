27d36156cbf1ba4ba8d5bf3f376b340bec36b691
SoWhoYou <sowhoyou@gmail.com>
2022-07-14 17:32:42 -0400

    updates

--- assets/js/document-ready.js
+++ assets/js/document-ready.js
@@ -1,10 +1,14 @@
-// $(window).on('load', function () {
+$(window).on('load', function () {
 
-//     setTimeout(function() {
+    setTimeout(function() {
+
+        if(window.location.href.indexOf("?") > -1) {
+            alert(window.location.href);
+        }
 
         // $("#menu").css("width", "50px");
         // $("#menu").css("opacity", "0.5");
 
-//     }, 100);
+    }, 100);
 
-// });
+});

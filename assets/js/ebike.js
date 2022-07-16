1d0974ca9196b1bda96c09417f5a178ac74b9f26
SoWhoYou <sowhoyou@gmail.com>
2022-07-16 02:44:40 -0400

    updates

--- assets/js/ebike.js
+++ assets/js/ebike.js
@@ -26,40 +26,45 @@ function replaceAge() {
     var nowYear = now.getFullYear();
     var pastYear = past.getFullYear();
     var age = nowYear - pastYear;
-    return age;
+    $("#age").replaceWith(age);
 }
 
 function replaceUpdated() {
     var jqxhr = $.getJSON( "https://api.github.com/repos/MiahFuta/miahfuta.github.io/commits?path=ebike.html&page=1&per_page=1", function() {
-        console.log( "success" );
+        console.log( "API Request Success" );
     })
     .done(function() {
         var time = jqxhr.responseJSON[0]['commit']['committer']['date'];
-        newtime = new Date(time);     
-        let hours = newtime.getHours();
-        let minutes = newtime.getMinutes();
-        let ampm = hours >= 12 ? 'PM' : 'AM';
-        hours = hours % 12;
-        hours = hours ? hours : 12;
-        minutes = minutes.toString().padStart(2, '0');
-        let strTime = hours + ':' + minutes + ' ' + ampm;
-        var reply = 'Last Updated: ' + (newtime.getMonth() + '/' + newtime.getDate() + '/' + newtime.getFullYear());
-        $("#lastUpdated").html(reply + ' at ' + strTime);
+        newtime = new Date(time);    
+        if (newtime instanceof Date && !isNaN(newtime)) {
+            var month = newtime.getMonth();
+            var day = newtime.getDate();
+            var year = newtime.getFullYear();
+            var theDate = (month + '/' + day + '/' + year);
+            var hours = newtime.getHours();
+            var minutes = newtime.getMinutes();
+            var ampm = hours >= 12 ? 'PM' : 'AM';
+            hours = hours % 12;
+            hours = hours ? hours : 12;
+            minutes = minutes.toString().padStart(2, '0');
+            var theTime = hours + ':' + minutes + ' ' + ampm;
+            $("#lastUpdated").html('Last Updated: ' + theDate + ' at ' + theTime);
+        } else {
+            $("#lastUpdated").replaceWith('');
+        }
     })
     .fail(function() {
-        console.log( "error" );
+        console.log( "API Request Error" );
     })
     .always(function() {
-        console.log( "complete" );
+        console.log( "API Request Complete" );
     });
 }
 
 $(window).on('load', function () {
     setTimeout(function() {
-
-        $("#age").replaceWith(replaceAge());
+        replaceAge();
         replaceUpdated();
-
     }, 100);
 });
 
